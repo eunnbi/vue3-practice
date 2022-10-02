8e184fcefd52a09205dcd631708ad11f23657938
Eunbi Kang <jenabill@naver.com>
2022-10-02 17:52:03 +0900

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-center font-bold">{{ msg }}</h1>
+  </div>
+</template>
---
+++ src/components/layout/Nav.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="flex gap-5">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style>
+a.router-link-exact-active {
+  font-weight: bold;
+  color: green;
+}
+</style>
