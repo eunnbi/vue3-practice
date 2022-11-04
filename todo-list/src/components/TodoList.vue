52a4ae456b8cedd1e3b6a38b1e1445d99f0f9d3a
Eunbi Kang <jenabill@naver.com>
2022-11-04 23:06:13 +0900

    add(todo-list): TodoList component

---
+++ todo-list/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useTodosStore } from "@/stores/todos";
+
+interface Props {
+  todos: Todo[];
+}
+const { todos } = defineProps<Props>();
+const { removeTodo, changeDoneState } = useTodosStore();
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.title }}
+      <button v-if="!todo.done" @click="changeDoneState(todo.id)">V</button>
+      <button @click="removeTodo(todo.id)">X</button>
+    </li>
+  </ul>
+</template>
+
+<style>
+li {
+  text-transform: capitalize;
+}
+</style>
