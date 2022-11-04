4dd0edee442a44fab9fefbc392dc200a1d6403ed
Eunbi Kang <jenabill@naver.com>
2022-11-04 23:06:05 +0900

    add(todo-list): TodoForm component

---
+++ todo-list/src/components/TodoForm.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useTodosStore } from "@/stores/todos";
+import { ref } from "vue";
+const newTodo = ref("");
+const { addTodo } = useTodosStore();
+
+const onSubmit = () => {
+  addTodo(newTodo.value);
+  newTodo.value = "";
+};
+</script>
+
+<template>
+  <form v-on:submit.prevent="onSubmit">
+    <input v-model="newTodo" placeholder="Enter todo" />
+    <button>Add</button>
+  </form>
+</template>
