f96fc5a76e6576aca35cf7952f88e602afda0dd9
Eunbi Kang <jenabill@naver.com>
2022-11-09 19:53:00 +0900

    add: bomb project

---
+++ bomb/src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const settingTime = 5;
+const initialMessage = "Ready to bomb😊";
+const initialTime = 0;
+const time = ref(initialTime);
+const message = ref(initialMessage);
+let timerId: number;
+
+const start = () => {
+  timerId = setInterval(() => {
+    time.value++;
+    if (time.value == settingTime) {
+      message.value = "Baaaaaaaaamm😱";
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
+
+const stop = () => {
+  clearInterval(timerId);
+}
+
+const reset = () => {
+  time.value = initialTime;
+  message.value = initialMessage;
+}
+</script>
+
+<template>
+  <p>Set Time : {{ settingTime }}</p>
+  <button type="button" @click="start">start</button>
+  <button type="button" @click="stop">stop</button>
+  <button type="button" @click="reset">reset</button>
+  <div class="circle">
+    <p>
+      {{ time }}
+    </p>
+  </div>
+  <p>{{ message }}</p>
+</template>
+
+<style scoped>
+.circle {
+    background-color: black;
+    color: white;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    margin-top: 20px;
+  }
+</style>
