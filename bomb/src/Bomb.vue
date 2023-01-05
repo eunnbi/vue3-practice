040eda12312938727a2d71fd95276a5b900e2005
Eunbi <jenabill@naver.com>
2023-01-05 21:50:50 +0900

    add(bomb): Bomb component

---
+++ bomb/src/Bomb.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { time } = defineProps({
+  time: Number,
+});
+</script>
+
+<template>
+  <div class="circle">
+    <p>
+      {{ time }}
+    </p>
+  </div>
+</template>
+
+<style>
+.circle {
+  background-color: black;
+  color: white;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  margin-top: 20px;
+}
+</style>
